4f1cf2d91dd5a777f1c4f7a87fc56f548ee30373
zachvlat <zachariasvlatakis@gmail.com>
2024-08-01 14:49:54 +0300

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,87 @@
+[
+  {
+    "name": "Nextcloud",
+    "description": "File sync and share software for individuals to enterprises.",
+    "link": "https://nextcloud.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/nextcloud.png"
+  },
+  {
+    "name": "Portainer",
+    "description": "Hybrid multi-cloud, multi-cluster container management.",
+    "link": "https://portainer.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/portainer.png"
+  },
+  {
+    "name": "Filebrowser",
+    "description": "Manage, upload, delete, and edit files within a directory.",
+    "link": "https://files.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/filebrowser.png"
+  },
+  {
+    "name": "Jellyfin",
+    "description": "Free media server to organize and share digital media files.",
+    "link": "https://jellyfin.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/jellyfin.png"
+  },
+  {
+    "name": "Transmission",
+    "description": "BitTorrent client with various user interfaces.",
+    "link": "https://transmission.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/transmission.png"
+  },
+  {
+    "name": "FreshRSS",
+    "description": "Self-hosted RSS and Atom feed aggregator.",
+    "link": "https://rss.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/freshrss.png"
+  },
+  {
+    "name": "deemix",
+    "description": "Deezer downloader library built from Deezloader Remix.",
+    "link": "https://deemix.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/deemix.png"
+  },
+  {
+    "name": "HomeAssistant",
+    "description": "Control your devices with Home Assistant apps.",
+    "link": "https://ha.zatspi.duckdns.org",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/homeassistant.png"
+  },
+  {
+    "name": "AudiobookSelf",
+    "description": "Organize and listen to your audiobooks.",
+    "link": "https://audiobook.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/audiobookself.png"
+  },
+  {
+    "name": "Navidrome",
+    "description": "Music server and streaming service.",
+    "link": "https://navidrome.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/navidrome.png"
+  },
+  {
+    "name": "Bitwarden",
+    "description": "Open-source password management solutions.",
+    "link": "https://bitwarden.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/bitwarden.png"
+  },
+  {
+    "name": "NGINX Proxy Manager",
+    "description": "Manage your NGINX proxy hosts with ease.",
+    "link": "https://zatspi.duckdns.org/nginx/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/nginxproxymanager.png"
+  },
+  {
+    "name": "Podgrab",
+    "description": "Podcast download and management tool.",
+    "link": "https://podgrab.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/podgrab.png"
+  },
+  {
+    "name": "Uptime Kuma",
+    "description": "Uptime Kuma is an easy-to-use self-hosted monitoring tool.",
+    "link": "https://kuma.zatspi.duckdns.org/",
+    "icon": "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/uptimekuma.png"
+  }
+]
+
